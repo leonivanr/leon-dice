3749d9dded2fcb67e71122663e8f8b82995e12b3
Leon <leonivanrodrigo@gmail.com>
2019-05-25 02:20:10 -0300

    Fin maquetado.

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 let secuencia = [];
 let secuenciaJugador = [];
 let luz;
-let turnoUser;
-let correcto;
-let intervalo;
 let turnoCpu;
-let ruido = false;
-let escricto = false;
-let on = false;     
 let ganador;
+var contador = 0;
+
+
+const contadorRacha = document.querySelector('#contador');
+const racha = document.querySelector('#racha');
+const jugarBtn = document.querySelector('#jugar');
+jugarBtn.addEventListener('click', comenzar);
+const botones = Array.from(document.querySelectorAll('.casillas'));
+const activarBotones = () => botones.forEach(boton => boton.addEventListener('click', clickFn));
+const pausarBotones = () => botones.forEach(boton => boton.removeEventListener('click', clickFn));
+
+
+
+function clickFn(e) {
+    pausarBotones();
+    let botonPresionado = e.target.id;
+    const clip = document.querySelector('#' + botonPresionado + '-snd');
+    clip.play();
+    const boton = document.querySelector('#' + botonPresionado);
+    boton.style.cursor = 'pointer';
+    boton.classList.toggle(botonPresionado + '-click');
+    setTimeout(() => {
+        boton.classList.toggle(botonPresionado + '-click');
+        activarBotones();
+    }, 700);
+    contador++;
+    
+    racha.innerHTML = contador;
+}
+
+function comenzar() {
+    botones.forEach(boton => boton.style.cursor = 'pointer')
+    activarBotones();
+    const boton = document.querySelector('#jugar');
+    boton.style.display = 'none';
+    contadorRacha.style.display = 'block';
+}
